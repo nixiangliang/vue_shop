3765102eb093ef3bb8c5a02d93f8a8ac6ea9de00
倪祥亮 <1453576498@qq.com>
2022-02-18 16:41:25 +0800

    完成了用户列表功能的开发

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -54,13 +54,14 @@ export default {
     resetLoginForm() {
       this.$refs.loginFormRef.resetFields()
     },
+    // 点击登录按钮，实现登录功能
     login() {
       this.$refs.loginFormRef.validate(async (valid) => {
         if (!valid) return
         const { data: res } = await this.$http.post('login', this.loginForm)
         if (res.meta.status !== 200) return this.$message.error('登陆失败！')
         this.$message.success('登陆成功！')
-        console.log(res)
+        // console.log(res)
         // 1. 将登录成功之后的 token ，保存到客户端的 sessionStorage 中
         //   1.1 项目中除了登录之外的其它API接口，必须在登录之后才能访问
         //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
--- src/main.js
+++ src/main.js
@@ -10,6 +10,14 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 配置请求拦截器
+axios.interceptors.request.use((config) => {
+  // 在 API 接口的请求头中使用 Authorization 字段提供 token 令牌
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 // 全局挂载 axios
 Vue.prototype.$http = axios
 
